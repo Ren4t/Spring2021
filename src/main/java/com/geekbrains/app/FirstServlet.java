a2d32e834577a3eae9dfb823f6123a0f0be80ad7
Ren4t <ren4tab@yandex.ru>
2021-06-27 15:24:30 +0300

    first commit master

---
+++ src/main/java/com/geekbrains/app/FirstServlet.java
@@ -0,0 +1,35 @@
+package com.geekbrains.app;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+// http://localhost:8080/client2/first_servlet
+public class FirstServlet extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(FirstServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().printf("<html><body>");
+        for (int i = 0; i < 10; i++) {
+            resp.getWriter().printf("<h1>" + new Product(1001+i,"title" + (1+i),997+i*370+155*i).toString() + "</h1>");
+        }
+        resp.getWriter().printf("</body></html>");
+    }
+
+    @Override
+    public void destroy() {
+        logger.debug("Destroy");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        logger.debug("Init");
+    }
+}
